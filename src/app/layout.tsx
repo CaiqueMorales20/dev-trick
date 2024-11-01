99cdabf72959c7afed008e9235049ecb470739cc
CaiqueMorales20 <caiquemorales20@gmail.com>
2024-11-01 17:02:27 -0300

    feat: add environment

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+})
+
+export const metadata: Metadata = {
+  title: 'Dev Trick',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>{children}</body>
+    </html>
+  )
+}
